915752901a8d6090fb5de9961d63f817d2f03e8c
ie1404 <20230757@mywhitecliffe.com>
2023-03-16 11:48:38 +1300

    update

--- oop.py
+++ oop.py
@@ -3,6 +3,13 @@ class cat():
     age:None
     ishappy:None
 
+    # constructor
+    def __init__(self, name,age,ishappy ):
+        self.name=name
+        self.age=age
+        self.ishappy=ishappy
+
+
     def display(self):
         print("CAT")
         print("name", self.name)
@@ -10,10 +17,13 @@ class cat():
         print("ishappy",self.ishappy)
     def sound(self):
         print("Meow")
-cat1=cat()
-cat.name="jack;"
-cat.age=6
-cat.ishappy=True
+#this is the same as-
+cat1=cat("jack,6,true")
+#this down below
+
+# cat.name="jack;"
+# cat.age=6
+# cat.ishappy=True
 
 cat1.display()
 cat1.sound()    
